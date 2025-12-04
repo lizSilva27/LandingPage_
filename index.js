a000a190531bda6eb34b0d7ba34bc54632d03f96
lizSilva27 <silvalizeth647@gmail.com>
2025-12-04 16:55:09 -0600

    Update cambio de contenido dinamicamente

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   const menuBtn = document.getElementById("btnMenu");
   const navMenu = document.getElementById("navMenu");
+
+  const items = document.querySelectorAll(".navContainer__containerLinks__links");
+  const sections = document.querySelectorAll(".dynamicContainer");
+
+  items.forEach(item => {
+    item.addEventListener("click", () => {
+      const target = item.dataset.target;
+
+      //Quitar activos del menú
+      items.forEach(i => i.classList.remove("active"));
+      item.classList.add("active");
+
+      //Cambiar contenido
+      sections.forEach(sec => {
+        sec.classList.toggle("active", sec.dataset.section === target);
+      });
+    });
+  });
 
   menuBtn.addEventListener("click", () => {
     navMenu.classList.toggle("active");
